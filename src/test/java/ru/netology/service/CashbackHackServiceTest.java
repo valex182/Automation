c30bbb74a64a6e843174ef78e9a78e86bbefc09e
Aleksandr Voronovich <vsanya93@mail.ru>
2022-03-28 18:20:30 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,57 +1,65 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void shouldCalculateRemain() {
+    public void shouldCalculateRemain() {
         int amount = 600;
         int actual = service.remain(amount);
         int expected = 400;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateRemainBoundary() {
+    public void shouldCalculateRemainBoundary() {
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateRemainBoundary2() {
+    public void shouldCalculateRemainBoundary2() {
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateRemainBoundary3() {
+    public void shouldCalculateRemainBoundary3() {
         int amount = 1;
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateRemainWhen0() {
+    public void shouldCalculateRemainWhen0() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateRemainWhen1000() {
+    public void shouldCalculateRemainWhen1000() {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFail() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 }
