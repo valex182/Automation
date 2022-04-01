cf5b7d5ad6630dcab47e90bb580f0e44982bde22
Aleksandr Voronovich <vsanya93@mail.ru>
2022-04-01 15:22:46 +0300

    fix homework

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -51,7 +51,7 @@ public class CashbackHackServiceTest {
     void shouldCalculateRemainWhen1000() {
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
